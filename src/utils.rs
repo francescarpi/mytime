cdb05c4846c1ed91cdbbd65d8bb8f3260fc704b5
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-05-30 10:01:17 +0200

    fix: wrong date format

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
 pub mod formatters {
-    use ::chrono::DateTime;
+    use ::chrono::{DateTime, Local};
 
     pub fn format_seconds(seconds: i64) -> String {
         let hours = seconds / 3600;
@@ -10,6 +10,7 @@ pub mod formatters {
     pub fn format_date(date_time: String) -> String {
         DateTime::parse_from_rfc3339(&date_time)
             .unwrap()
+            .with_timezone(&Local)
             .format("%Y-%m-%d %H:%M")
             .to_string()
     }
