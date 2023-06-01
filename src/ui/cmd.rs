af8aa832a94919f80f9f6dc51f4beada48f91689
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-06-01 08:27:30 +0200

    feat: commands refactored

---
+++ src/ui/cmd.rs
@@ -0,0 +1,69 @@
+use clap::{Arg, ArgMatches, Command};
+
+pub fn command() -> ArgMatches {
+    Command::new("mytime")
+        .author("Francesc Arpí Roca")
+        .version("0.1.") // get from cargo
+        .about("Program to tracker your working time")
+        .subcommand(
+            Command::new("start").about("Start a new task").arg(
+                Arg::new("desc")
+                    .short('d')
+                    .help("Description")
+                    .required(true)
+                    .value_parser(clap::value_parser!(String)),
+            ),
+        )
+        .subcommand(Command::new("stop").about("Stop de active task"))
+        .subcommand(
+            Command::new("show")
+                .about("Display the tasks table")
+                .arg(
+                    Arg::new("period")
+                        .short('p')
+                        .conflicts_with_all(&["relative", "date"])
+                        .value_parser(["today", "week", "month"]),
+                )
+                .arg(
+                    Arg::new("relative")
+                        .short('r')
+                        .conflicts_with_all(&["period", "date"])
+                        .help("1 == -1 == yesterday")
+                        .value_parser(clap::value_parser!(u8).range(0..=7)),
+                )
+                .arg(
+                    Arg::new("date")
+                        .short('d')
+                        .conflicts_with_all(&["relative", "period"])
+                        .help("Format: YYYY-MM-DD"),
+                ),
+        )
+        .subcommand(
+            Command::new("modify")
+                .about("Modify a task's description")
+                .arg(
+                    Arg::new("id")
+                        .short('i')
+                        .help("Task ID")
+                        .value_parser(clap::value_parser!(i64))
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("desc")
+                        .short('d')
+                        .help("Description")
+                        .value_parser(clap::value_parser!(String))
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            Command::new("reopen").about("Reopen a closed task").arg(
+                Arg::new("id")
+                    .short('i')
+                    .help("Task ID")
+                    .value_parser(clap::value_parser!(i64))
+                    .required(true),
+            ),
+        )
+        .get_matches()
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,2 +1,2 @@
-pub mod cli;
+pub mod cmd;
 pub mod actions;
